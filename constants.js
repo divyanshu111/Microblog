ef257fb86c00d423eedd5bed20c5fd0eae7364ff
divyanshu111 <40720319+divyanshu111@users.noreply.github.com>
2022-03-26 19:01:36 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
       SESSIONKEY :"feewfwwrweq12342" ,
       port :3000 ,
-      DB: 'mongodb+srv://DivyanshuSen:patakoin@cluster0.nfpv7.mongodb.net/accioblogs?retryWrites=true&w=majority'
+      DB: 'mongoUrl'
    });
